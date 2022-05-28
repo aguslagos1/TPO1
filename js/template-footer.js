030ad9cafaef61bb50112ca2724123cd5fb39515
aguslagos1 <106122771+aguslagos1@users.noreply.github.com>
2022-05-28 01:13:18 -0300

    Add files via upload

---
+++ js/template-footer.js
@@ -0,0 +1,20 @@
+document.body.innerHTML+=' <footer> \
+<div class="container_footer"> \
+    <div> \
+        <ul> \
+            <li>Navegación</li> \
+            <li><a href="#">Home</a></li> \
+            <li><a href="parts.html">Servicios</a></li> \
+            <li><a href="contact.html">Contacto</a></li> \
+        </ul> \
+    </div> \
+    <div> \
+        <h3>Horarios de Atención</h3> \
+        <p>Lunes a viernes 8-18 hs.</p> \
+    </div> \
+    <div> \
+        <p>Atención al cliente:</p> \
+        <span><i class="fa-solid fa-headset"></i><a href="wa.me/541121908869">+54 11 2190-8869</a></span> \
+    </div> \
+</div> \
+</footer> ';
